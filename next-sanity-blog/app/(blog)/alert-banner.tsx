00367101e8fc76b924527e332a7194ab3bf69648
RizwanaAwan123 <awanareeb34@gmail.com>
2025-01-08 20:24:52 +0500

    first commit

--- next-sanity-blog/app/(blog)/alert-banner.tsx
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -1,23 +1,6 @@
-// "use client";
-
-// import { useRouter } from "next/navigation";
-// import { useSyncExternalStore, useTransition } from "react";
-
-// import { disableDraftMode } from "./actions";
-
-// const emptySubscribe = () => () => {};
+import Link from "next/link";
 
  export default function AlertBanner() {
-//   const router = useRouter();
-//   const [pending, startTransition] = useTransition();
-
-//   const shouldShow = useSyncExternalStore(
-//     emptySubscribe,
-//     () => window.top === window,
-//     () => false,
-//   );
-
-//   if (!shouldShow) return null;
 
   return (
     <div
@@ -26,6 +9,11 @@
       <div className="py-2 text-center text-sm">
          BLOGS
          </div>
+         <nav className="flex space-x-6 text-sm font-medium">
+      <Link href="/" className=" hover:text-blue-500">Home</Link>
+      <Link href="/about"className=" hover:text-blue-500">About</Link>
+      <Link href="/contact"className=" hover:text-blue-500">Contact</Link>
+    </nav>
        </div>
   );
 }
